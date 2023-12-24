2acd43715ac3157d20a2e09f9cfdc8d4056f9cfa
fikrialnfjr <fikrialnfjr@gmail.com>
2023-12-24 21:14:56 +0700

    Final

--- overview.py
+++ overview.py
@@ -14,7 +14,7 @@ def show_overview():
     faktor-faktor yang memengaruhi ketahanan pangan dan berpotensi memberikan wawasan berharga untuk kebijakan dan strategi mitigasi.
     """)
 
-    st.subheader("Goals Project")
+    st.subheader("Goals")
 
     st.write("""
     Tujuan proyek ini adalah mengembangkan model prediktif untuk mengukur tingkat ketahanan dan kerentanan pangan 
@@ -39,3 +39,71 @@ def show_overview():
     st.subheader("Workflow Modelling")
     local_image_path_2 = "/Users/fikrialinfijar/Developer/FSVA/2.png"
     st.image(local_image_path_2, caption="Workflow Modelling", use_column_width=True)
+
+    st.write("#### **Pengumpulan Dataset**")
+    st.write("""
+    Dataset yang digunakan berasal web Badan Pangan Nasional (BPN) : 
+    https://fsva.badanpangan.go.id
+    """)
+
+    st.write("#### **Preprocessing**")
+    st.write("""
+    Preprocessing yang dilakukan hanya menghapus kolom yang tidak digunakan pada saat modelling.
+    """)
+
+    st.write("#### **Featuring Engineering**")
+    st.write("""
+    **Featuring Engineering menggunakan StandardScaler**
+
+    StandardScaler adalah metode normalisasi yang digunakan untuk mentransformasi data sehingga setiap fitur memiliki rata-rata 0 
+    dan deviasi standar 1. Hal ini membantu menghilangkan perbedaan skala antar fitur, yang dapat mempengaruhi performa 
+    algoritma machine learning, terutama yang sensitif terhadap skala seperti regresi logistik dan support vector machines (SVM).
+    """)
+
+    st.write("#### **Modelling**")
+    st.write("##### Supervised Learning")
+    st.write("""
+    - **Random Forest**
+
+    Random Forest adalah algoritma ensemble yang membangun sejumlah pohon keputusan selama pelatihan dan menggabungkan 
+    hasilnya. Ini termasuk konsep pengambilan sampel acak dari setiap data pelatihan dan konstruksi pohon keputusan 
+    independen satu sama lain. Random Forest memiliki kinerja yang baik untuk tugas klasifikasi dan regresi, serta 
+    toleran terhadap overfitting.
+
+    Model Random Forest terbaik yang kami buat adalah : 
+    
+    RandomForestClassifier(n_estimators=100, random_state=42)
+
+    dimana akurasinya sebagai berikut : 
+
+        \t- Pada data train\t: 1.0
+
+        - Pada data test\t: 0.8349514563106796
+
+    - **SVM**
+
+    SVM adalah algoritma pembelajaran mesin yang digunakan untuk tugas klasifikasi dan regresi. SVM mencari pemisah optimal 
+    (hyperplane) antara dua kelas dengan memaksimalkan margin antara kedua kelas. SVM dapat bekerja baik dengan data yang 
+    memiliki dimensi tinggi dan sangat baik untuk tugas klasifikasi, terutama dalam kasus ketika ada kebutuhan untuk 
+    menangani data yang tidak linier.
+
+    Model Random Forest terbaik yang kami buat adalah : 
+    
+    SVC(kernel='rbf', C=1.0, gamma='scale', random_state=42)
+
+    dimana akurasinya sebagai berikut : 
+
+        \t- Pada data train\t: 0.8880778588807786
+
+        - Pada data test\t: 0.8349514563106796
+    """)
+
+    st.write("##### Unupervised Learning")
+    st.write("""
+    **K-Means Clustering**
+
+    K-Means Clustering adalah algoritma pembelajaran mesin yang digunakan untuk mengelompokkan data menjadi beberapa kelompok 
+    (cluster) berdasarkan kesamaan fitur. Algoritma ini mencoba untuk membagi data menjadi k kelompok dengan cara meminimalkan 
+    varians di dalam setiap kelompok. Setiap kelompok ditentukan oleh pusat massa atau centroid, dan setiap data diatribusikan 
+    ke kelompok dengan pusat massa terdekat.
+    """)
